d99044734537e0b7e0296628574b3c9c829575d3
AkilaRanasinghe <akilaranasingha41@gmail.com>
2020-10-13 00:14:45 +0530

    Akila ge wada karana eka version 3

--- app/models.py
+++ app/models.py
@@ -85,6 +85,8 @@ class Salary (models.Model):
 
 class Attendance (models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="employeeid", null=False)
+    date = models.DateField(null=False)
+    type = models.CharField(max_length=200, null=True)
     outTime = models.CharField(max_length=200, null=True)
     inTime = models.CharField(max_length=200, null=True)
 
